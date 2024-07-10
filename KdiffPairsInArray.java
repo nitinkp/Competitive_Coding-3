0c2ab7e6c7e6d494c6e44bc8500d565054cd02df
Nithin Kumar Pamidimukkala <nik@Nithins-MBP.lan>
2024-07-10 15:10:04 -0400

    Completed Competitive_Coding-3 ammend

--- KdiffPairsInArray.java
+++ KdiffPairsInArray.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class KdiffPairsInArray {
+import java.util.HashMap;
+/*
+https://leetcode.com/problems/k-diff-pairs-in-an-array/description/
+ */
+public class KdiffPairsInArray {
+    public static int findPairs(int[] nums, int k) {
+        HashMap<Integer, Integer> hmap = new HashMap<>();
+        int count = 0;
+
+        for (int num : nums) { //O(n) T.C
+            if (!hmap.containsKey(num)) {
+                hmap.put(num, 1); //O(n) S.C
+            } else { //building a frequency hashmap
+                hmap.put(num, hmap.get(num) + 1);
+            }
+        }
+
+        for(int i : hmap.keySet()) { //O(1) T.C
+            if(hmap.containsKey(i+k) && k>0) { //if hashmap contains both i and i+difference
+                count++; //that means we found a pair
+            }
+            else if(hmap.get(i) > 1 && k==0) { //if given target is 0 and if any value has a duplicate
+                count++; //that also counts as a pair
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,-1,0,1,2,4,-3,-2,4,3,0};
+        int k = 3;
+        System.out.println("count of " + k + " diff pairs in given nums is "
+                + findPairs(nums, k));
+    }
 }
--- PascalsTriangle.java
+++ PascalsTriangle.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class PascalsTriangle {
+import java.util.ArrayList;
+import java.util.List;
+/*
+https://leetcode.com/problems/pascals-triangle/
+ */
+public class PascalsTriangle { //O(n*n) T.C, O(1) S.C
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>(); //initialize the outer list
+        for(int i=0; i<numRows; i++) { //accessing the lists in outer list
+            List<Integer> list = new ArrayList<>(); //initialize the inner list each time for given numrows
+            for(int j=0; j<=i; j++) { //accessing the values in inner list
+                if(j == 0 || j == i) list.add(1); //if the element is either of the corner, just insert 1
+                else { //if this is a middle element
+                    int leftAbove = res.get(i-1).get(j-1); //left value is at one row above and one colum left
+                    int rightAbove = res.get(i-1).get(j); //right value is at one row above and same column
+                    list.add(leftAbove + rightAbove); //adding the above two gives current row value
+                }
+            }
+            res.add(list); //add the list at end of each iteration
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int num = 7;
+        System.out.println("The pascal triangle for rows " + num + " is: ");
+        List<List<Integer>> output = generate(num);
+        for (List<Integer> integers : output) {
+            System.out.println(integers);
+        }
+    }
 }
